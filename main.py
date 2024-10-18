655d28ca5552cc8267000358235dede3f30370dc
Vlad Sevruk <vladworking5@gmail.com>
2024-10-18 15:16:43 +0300

    №5

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ string=input("Введите строку:")#ввод строки
 words=string.split()#инициализация переменной words
 string1=''#инициализация переменной string
 for word in words:#цикл for
-        string1 += word[0]#инициализация превых букв
+        string1 += word[0]#инициализация первых букв
 
 print("Результат:",string1)#вывод результата
